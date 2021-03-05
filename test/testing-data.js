2b2a67a2d7a07b59c6d71efbf4f7ccc79779ac0a
Aaron Foucheaux <afoucheaux@gmail.com>
2021-03-04 19:11:50 -0600

    Update to testing data

--- test/testing-data.js
+++ test/testing-data.js
@@ -1,27 +1,27 @@
 const customers = [
-  {"id":1, "name":"Leatha Ullrich"},
-  {"id":2, "name":"Rocio Schuster"},
-  {"id":3, "name":"Kelvin Schiller"},
-  {"id":4, "name":"Kennedi Emard"},
-  {"id":5, "name":"Rhiannon Little"},
-  {"id":6, "name":"Fleta Schuppe"},
-  {"id":7, "name":"Dell Rath"},
-  {"id":8, "name":"Era Hand"},
-  {"id":9, "name":"Faustino Quitzon"},
-  {"id":10, "name":"Tony Armstrong"}
+  {"id":1, "name": "Person One"},
+  {"id":2, "name": "Person Two"},
+  {"id":3, "name": "Person Three"},
+  {"id":4, "name": "Person Four"},
+  {"id":5, "name": "Person Five"},
+  {"id":6, "name": "Person Six"},
+  {"id":7, "name": "Person Seven"},
+  {"id":8, "name": "Person Eight"},
+  {"id":9, "name": "Person Nine"},
+  {"id":10, "name": "Person Ten"}
 ];
 
 const rooms = [
-  {"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":358.4},
-  {"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},
-  {"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},
-  {"number":4,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":429.44},
-  {"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.17}
-  {"number":6,"roomType":"junior suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":397.02},
-  {"number":7,"roomType":"junior suite","bidet":false,"bedSize":"king","numBeds":2,"costPerNight":496.41},
-  {"number":8,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":374.67},
-  {"number":9,"roomType":"residential suite","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":343.95},
-  {"number":10,"roomType":"single room","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":429.32},
+  {"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":350.50},
+  {"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":475.50},
+  {"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":490.75},
+  {"number":4,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":430.25},
+  {"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.15}
+  {"number":6,"roomType":"junior suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":400.00},
+  {"number":7,"roomType":"junior suite","bidet":false,"bedSize":"king","numBeds":2,"costPerNight":495.50},
+  {"number":8,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":375.55},
+  {"number":9,"roomType":"residential suite","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":340.00},
+  {"number":10,"roomType":"single room","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":430.99},
 ];
 
 const bookings = [
