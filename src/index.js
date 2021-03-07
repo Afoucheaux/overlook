b47023c1c3ff32c0056ad1de4683db6d8ed67b1f
Aaron Foucheaux <afoucheaux@gmail.com>
2021-03-07 07:22:23 -0600

    Feature user dashboard

--- src/index.js
+++ src/index.js
@@ -23,8 +23,10 @@ let bookingRepo= [];
 let userRepo = [];
 let roomRepo = [];
 let todayDate = '2021/03/06';
-const headerName = document.querySelector('#headerName');
-
+const headerName = document.getElementById('headerName');
+const userBookings = document.getElementById('userPast');
+const bookingArea = document.getElementById('bookingArea');
+const custSpent = document.getElementById('custSpent')
 // ---- fetch functions and page builds ----
 
 Promise.all([getRoomsData, getBookingsData, getCustomersData])
@@ -35,6 +37,7 @@ Promise.all([getRoomsData, getBookingsData, getCustomersData])
     userLogIn('Leatha Ullrich', 'overlook2021');
   })
 
+// --- function ---
 function buildRoomsData(roomsObj) {
   roomRepo = new RoomRepo();
   roomsObj.rooms.forEach(room => {
@@ -57,17 +60,31 @@ function buildCustomersData(CustomersData) {
 }
 
 function userLogIn(name, password) {
-  const checkUser = userRepo.allUsers.find(user => user.name === name);
-  if (checkUser.password === password) {
-    user = checkUser
-    buildDashboard()
+  user = userRepo.allUsers.find(user => user.name === name);
+  if (user.password === password) {
+    user.findBookings(bookingRepo);
+    buildDashboard();
   }
 }
 
 function buildDashboard() {
   headerName.innerText = user.name;
+  userBookings.innerText = `${user.name} Bookings`
+  displayUserBookings(user.bookings, bookingArea);
+  updateCustomerSpent();
 }
 
+function displayUserBookings(array, displayElemt) {
+  displayElemt.innerHTML = ""
+  array.forEach(booking => {
+    displayElemt.insertAdjacentHTML( 'afterbegin', `<option class="bookingList" id="${booking.id}" value="default">${booking.date} room ${booking.roomNumber}</option>`)
+  })
+}
+
+function updateCustomerSpent() {
+  let spend = user.totalSpent(roomRepo);
+  custSpent.innerText =`${user.name} total $${spend}!`
+}
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
