df88ea247b94340aef38e87acee5ece669bf144b
Aaron Foucheaux <afoucheaux@gmail.com>
2021-03-07 09:20:40 -0600

    feature filter by room number

--- src/RoomRepo.js
+++ src/RoomRepo.js
@@ -2,6 +2,20 @@ class RoomRepo {
   constructor() {
     this.allRooms = [];
   }
+
+  filterByNumber(numList, roomList) {
+    let newList = [];
+    numList.forEach(num => {
+      let room = roomList.find(roomNum => roomNum.number === num);
+      newList.push(room)
+    })
+    return newList;
+  }
+
+  
+
+
+
 }
 
 export default RoomRepo;
--- test/roomRepo-test.js
+++ test/roomRepo-test.js
@@ -24,15 +24,32 @@ describe('RoomRepo', function() {
 
   it('Should be able to store a booking', function() {
     const roomRepo = new RoomRepo();
-    const room1 = new Room(roomsTestData[0]);
     const room2 = new Room(roomsTestData[1]);
     const room3 = new Room(roomsTestData[2]);
-    roomRepo.allRooms.push(room1);
+    roomRepo.allRooms.push(roomsTestData[0]);
     roomRepo.allRooms.push(room2);
     roomRepo.allRooms.push(room3);
     expect(roomRepo.allRooms.length).to.equal(3);
   })
 
+  describe('Room Filters', function() {
+
+    let roomRepo;
+    beforeEach(() => {
+      roomRepo = new RoomRepo;
+      roomsTestData.forEach(roomList => {
+        let room = new Room(roomList);
+       roomRepo.allRooms.push(room);
+      });
+    });
+
+    it('Should be able to filter rooms by number', function() {
+      const numList = [1, 2, 3, 4];
+      expect(roomRepo.filterByNumber(numList, roomRepo.allRooms).length).to.deep.equal(4)
+    });
+
+
+  });
 
 
 });
